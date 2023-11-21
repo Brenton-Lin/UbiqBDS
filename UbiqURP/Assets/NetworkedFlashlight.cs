4eef792759b89790e6fddffd8ba65bcaa9ee65d8
Brenton-Lin <brenton.lin@yahoo.com>
2023-11-20 22:34:03 -0500

    Revert "Snapzones for flashlight and utility belt in demoable state"
    This reverts commit 5b900a12dabf90ebac4d54515f85a15380820d45.

--- UbiqURP/Assets/NetworkedFlashlight.cs
+++ UbiqURP/Assets/NetworkedFlashlight.cs
@@ -1,94 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-using Ubiq.Messaging;
 using UnityEngine;
 
-public class NetworkedFlashlight : NetworkedObject
+public class NetworkedFlashlight : PhysicsObject
 {
     [SerializeField]
     private Flashlight flashlight;
     // Start is called before the first frame update
-    public void DoUse()
+    public override void DoUse()
     {
         // bool toggles on press and release
         flashlight.ClickClick();
         flashlight.ClickClick();
     }
-
-    public NetworkContext context;
-    public bool owner;
-    public bool use;
-    private Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        context = NetworkScene.Register(this);
-        rb = GetComponent<Rigidbody>();
-    }
-
-    Vector3 lastPosition;
-
-    void Update()
-    {
-        /*        Debug.Log("Last position: " + lastPosition);
-                Debug.Log("CurrentPosition: " + transform.position);*/
-
-        if (lastPosition != transform.localPosition)
-        {
-
-            lastPosition = transform.localPosition;
-            if (owner)
-            {
-                context.SendJson(new Message()
-                {
-                    position = transform.localPosition,
-                    rotation = transform.localRotation,
-                    clearOwners = false,
-                    isKinematic = true,
-                    use = use
-                });
-            }
-            else
-            {
-
-            }
-
-        }
-        else
-        {
-
-
-        }
-
-    }
-
-    private struct Message
-    {
-        public Vector3 position;
-        public Quaternion rotation;
-        public bool clearOwners;
-        public bool isKinematic;
-        public bool use;
-    }
-
-    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
-    {
-        // Parse the message
-        var m = message.FromJson<Message>();
-
-        // Use the message to update the Component
-        transform.localPosition = m.position;
-        transform.localRotation = m.rotation;
-        owner = m.clearOwners;
-        // Make sure the logic in Update doesn't trigger as a result of this message
-        lastPosition = transform.localPosition;
-        if (rb != null)
-        {
-            rb.isKinematic = m.isKinematic;
-        }
-
-        use = m.use;
-    }
-
 
 }
