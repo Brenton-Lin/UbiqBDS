4aebd958b37cd465a0b66ffa2c82a6b037df6917
Brenton-Lin <brenton.lin@yahoo.com>
2023-11-29 22:51:53 -0500

    Cleaned up unused resources and started ContourGenerator
    -deleted unused scenes removed unused assets
    -started a contour generator prototype that reads Unity terrain files and outputs to a PNG.

---
+++ UbiqURP/Assets/Scripts/ContourLines.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Windows;
+
+
+public class ContourLines : MonoBehaviour
+{
+    public Terrain terrain;
+
+    public int numberOfBands = 12;
+
+    public Color bandColor = Color.white;
+    public Color bkgColor = Color.clear;
+
+    public Renderer outputPlain;
+
+    public Texture2D topoMap;
+    public string outputPath;
+
+
+    void Start()
+    {
+        GenerateTopoLines();
+    }
+
+    void GenerateTopoLines()
+    {
+        //topoMap = ContourMap.FromTerrain( terrain );
+        //topoMap = ContourMap.FromTerrain( terrain, numberOfBands );
+        topoMap = ContourMap.FromTerrain(terrain, numberOfBands, bandColor, bkgColor);
+
+        if (outputPlain)
+        {
+            outputPlain.material.mainTexture = topoMap;
+        }
+        else
+        {
+            byte[] png = topoMap.EncodeToPNG();
+            File.WriteAllBytes(outputPath, png);
+        }
+    }
+}
